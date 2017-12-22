ff43768e60b53210f83ea2cfeced432144cd0cd1
rgreen3@hotmail.com <rob@200sx.org.uk>
2017-12-22 17:22:53 +0000

    ui improvements

---
+++ BarcodeManager/Converters/Minus10Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BarcodeManager.Converters
+{
+    public class Minus10Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double?) value - 10;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+}
